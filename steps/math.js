62d7872d40440c38dcbaa45f6b3383a29a3c19ea
negima <sortidocorp@gmail.com>
2023-01-02 20:40:29 +0100

    Chore: Change version of cucumber

--- steps/math.js
+++ steps/math.js
@@ -1,19 +1,20 @@
-const { defineSupportCode } = require("cucumber");
+const { Given, When, Then } = require("@cucumber/cucumber");
 const assert = require("assert");
 
-defineSupportCode(function ({ Given, Then, When }) {
-  let answer = 0;
+let answer = 0;
 
-  Given("I have a {int}", function (input) {
-    answer = input;
-  });
-  When("I add {int}", function (input) {
-    answer = answer + input;
-  });
-  Then("In total I have {int}", function (input) {
-    assert.equal(answer, input);
-  });
-  When("I multiply by {int}", function (input) {
-    answer = answer * input;
-  });
+Given("I have a {int}", function (input) {
+  answer = input;
+});
+
+When("I add {int}", function (input) {
+  answer = answer + input;
+});
+
+Then("In total I have {int}", function (input) {
+  assert.equal(answer, input);
+});
+
+When("I multiply by {int}", function (input) {
+  answer = answer * input;
 });
--- steps/postal-code.js
+++ steps/postal-code.js
@@ -1,23 +1,21 @@
 const axios = require("axios");
 
-const { defineSupportCode } = require("cucumber");
+const { Given, When, Then } = require("@cucumber/cucumber");
 
-defineSupportCode(function ({ Given, Then, When }) {
-  let answer = 0;
+let answer = 0;
 
-  Given("I have this postal code {int}", function (input) {
-    answer = input;
-  });
+Given("I have this postal code {int}", function (input) {
+  answer = input;
+});
 
-  When("I whant some information about the postal code", function () {
-    return axios
-      .get(`https://apicarto.ign.fr/api/codes-postaux/communes/${answer}`)
-      .then((res) => {
-        return res;
-      });
-  });
+When("I whant some information about the postal code", function () {
+  return axios
+    .get(`https://apicarto.ign.fr/api/codes-postaux/communes/${answer}`)
+    .then((res) => {
+      return res;
+    });
+});
 
-  Then("The response body should be", function (dataTable) {
-    return dataTable;
-  });
+Then("The response body should be", function (dataTable) {
+  return dataTable;
 });
